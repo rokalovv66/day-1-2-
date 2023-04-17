1ccfc028aceabe9dc50fff839b10f7e01c6a328b
Нисик <@DESKTOP-ALORV4C>
2023-04-17 13:55:28 +0300

    Добавьте файлы проекта.

---
+++ day 1 (2)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A = 3; // замените 3 на любое другое число
+        int A2 = A * A; // A^2
+        int A4 = A2 * A2; // A^4
+        int A8 = A4 * A4; // A^8
+
+        Console.WriteLine("A^2 = " + A2);
+        Console.WriteLine("A^4 = " + A4);
+        Console.WriteLine("A^8 = " + A8);
+    }
+}
